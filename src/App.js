dca0f3298dd3d7ea32d18277914ae3c78a88d661
Rajat-Jaisawal-8009 <158314751+Rajat-Jaisawal-8009@users.noreply.github.com>
2024-07-07 14:50:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,130 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { IoIosLogOut } from "react-icons/io";
+import { IoMdSend } from "react-icons/io";
+import React, {useEffect, useState} from 'react'
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut} from 'firebase/auth'
+import {app} from './firebase'
+import {addDoc, collection, getFirestore, orderBy, onSnapshot, serverTimestamp} from 'firebase/firestore'
+
+const auth = getAuth(app);
+
+const loginHandle = ()=>{
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+}
 
 function App() {
+
+  const [user, setUser] = useState(false)
+  const [messages,setMessages]=useState([])
+ 
+
+  useEffect(()=>{
+onAuthStateChanged(auth,(userData)=>{
+  setUser(userData)
+})
+  },[])
+
+const logout = ()=>{
+  signOut(auth)
+}
+
+////////////////////////////////
+
+const [inputVal, setInputVal]=useState({
+  username: "",
+  email: "",
+  mobile:""
+ })
+
+ const inputOnchange = (e)=>{
+   const value = e.target.value;
+   const name = e.target.name;
+ 
+   setInputVal({...inputVal,[name] : value})
+ }
+
+///////////////////////////////
+
+const db = getFirestore();
+useEffect(()=>{
+  const unsubscribe =onAuthStateChanged(auth,(data)=>{
+    console.log(data,'data')
+    setUser(data)
+  })
+  const unsubscribeForMessages= onSnapshot(collection(db,"Messages"),(snap)=>{
+
+    setMessages(snap.docs.map((item)=>{
+      const id=item.id;
+      return {id,...item.data()};
+    }))
+  })
+  return()=>{
+    unsubscribe();
+    unsubscribeForMessages();
+  }
+},[])
+
+
+const handleSubmit= async(e)=>{
+  e.preventDefault();
+  try{
+    await addDoc(collection(db,"Messages"),{
+      text:inputVal.username,
+      uid:user.uid,
+      uri:user.photoURL,
+      createdAt:serverTimestamp(),
+    })
+  }
+  catch(error){
+    alert(error)
+  }
+  setInputVal( {
+    username: "",
+    email: "",
+    mobile:""
+   })
+
+}
+
+
+
+
+console.log(user)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+  <div>
+    {user? 
+    <>
+
+    <div className='chat-Containet'>
+      <div className='chat-box'>
+      {
+        messages?.map((val)=>(
+          val.uid != "QaodAbzvLzepWcHV8cYjTgV87Bo1"? <div className='chat-text-box-admin'><span className='text-wrap'>{val.text}</span></div> : <div className='chat-text-box-user'><span className='text-wrap'>{val.text}</span></div>
+        ))
+      }
+
+<div className='form-wrap'>
+      <form className='form-box' onSubmit={(e)=>handleSubmit(e)}>
+      <input className='input-search'  type='text' placeholder='enter your message' value={inputVal.username} onChange={(e)=>inputOnchange(e)} name="username"/>
+     
+      <button  className='send-btn' type='submit'><IoMdSend /></button>
+      <button onClick={logout}  className='send-btn' type='submit'><IoIosLogOut /></button>
+      </form>
+      </div>
+      </div>
+
+      
     </div>
+    
+    </>
+    
+    :<button onClick={loginHandle}>Login</button>}
+  </div>
+  </>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUmbCZEGGn27pW_4eVGp9EbfMNVJhlJ6k",
+  authDomain: "chat-application-baae2.firebaseapp.com",
+  projectId: "chat-application-baae2",
+  storageBucket: "chat-application-baae2.appspot.com",
+  messagingSenderId: "772068113352",
+  appId: "1:772068113352:web:2e34c32d6b657fe697defd"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
